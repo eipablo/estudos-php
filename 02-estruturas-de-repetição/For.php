a216e8c1955db438909b7e872bba72d6c0f57236
Pablo Vinícius <pblo.vvi11@gmail.com>
2025-03-24 17:26:43 -0300

    Adiciona a pasta 02 - estruturas de repeticao

---
+++ 02-estruturas-de-repetição/For.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo for</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+    <div class="w3-container w3-center w3-animate-top w3-padding">
+        <h2 class="w3-container w3-teal">Escolha qual tabuada você deseja visualizar</h2>
+        <form class="w3-container " method="post" action="action.php">
+
+            <button class="w3-btn w3-margin w3-blue-grey w3-jumbo" name="btn0">0</button>
+            <button class="w3-btn w3-margin w3-blue-grey w3-jumbo" name="btn1">1</button>
+            <button class="w3-btn w3-margin w3-blue-grey w3-jumbo" name="btn2">2</button>
+            <button class="w3-btn w3-margin w3-blue-grey w3-jumbo" name="btn3">3</button>
+            <button class="w3-btn w3-margin w3-blue-grey w3-jumbo" name="btn4">4</button>
+            <button class="w3-btn w3-margin w3-blue-grey w3-jumbo" name="btn5">5</button>
+            <button class="w3-btn w3-margin w3-blue-grey w3-jumbo" name="btn6">6</button>
+            <button class="w3-btn w3-margin w3-blue-grey w3-jumbo" name="btn7">7</button>
+            <button class="w3-btn w3-margin w3-blue-grey w3-jumbo" name="btn8">8</button>
+            <button class="w3-btn w3-margin w3-blue-grey w3-jumbo" name="btn9">9</button>
+        </form>
+
+    </div>
+</body>
+</html>
--- Readme.md
+++ Readme.md
@@ -3,6 +3,7 @@ Repositório com meus exercícios de aprendizado em PHP.
 
 ## Estrutura  
 - `01-arrays/`: Exercícios com arrays.  
+- `02-estruturas-de-repetição/`: Exercícios com estruturas de repetição.  
 
 ## Como Usar  
 Clone o repositório e execute os arquivos em um servidor PHP local. 
